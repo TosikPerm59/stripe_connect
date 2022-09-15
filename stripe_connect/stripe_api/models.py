74d4722e8af212b54ff06a68d88fef0bb93a578d
Tosikperm59 <tosikperm59@gmail.com>
2022-09-16 00:28:45 +0500

    add urls.py in my app and create class Item in models.py and register this model in admin.py

--- stripe_connect/stripe_api/models.py
+++ stripe_connect/stripe_api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+
+    currency = {
+        'usd': 'usd',
+        'rub': 'rub'
+    }
+    name = models.CharField(max_length=30, verbose_name='Название товара')
+    description = models.CharField(max_length=100, verbose_name='Описание')
+    currency = models.CharField(max_length=3, verbose_name='Валюта', choices=currency)
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Цена')
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ['name']
+
+
+class Order(models.Model):
+    pass
