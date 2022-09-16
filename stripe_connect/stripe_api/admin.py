459c04835d5dcc4aba00c458cb5f8665e24bc031
Tosikperm59 <tosikperm59@gmail.com>
2022-09-16 13:31:57 +0500

    added class Order

--- stripe_connect/stripe_api/admin.py
+++ stripe_connect/stripe_api/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from models import Item
+from models import Item, Order
 
 
 class ItemAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'currency', 'price')
 
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('session', 'basket')
+
 
 admin.site.register(Item, ItemAdmin)
+admin.site.register(Order, OrderAdmin)
