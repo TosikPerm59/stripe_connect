472475f91adf1e95eab22c01b67d972df8098ab2
Tosikperm59 <tosikperm59@gmail.com>
2022-09-16 13:30:23 +0500

    added class Order

--- stripe_connect/stripe_api/models.py
+++ stripe_connect/stripe_api/models.py
@@ -7,7 +7,7 @@ class Item(models.Model):
         'rub': 'rub'
     }
     name = models.CharField(max_length=30, verbose_name='Название товара')
-    description = models.CharField(max_length=100, verbose_name='Описание')
+    description = models.CharField(max_length=100, verbose_name='Описание', blank=True, name=True)
     currency = models.CharField(max_length=3, verbose_name='Валюта', choices=currency)
     price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Цена')
 
@@ -18,4 +18,5 @@ class Item(models.Model):
 
 
 class Order(models.Model):
-    pass
+    session = models.CharField(max_length=30, verbose_name='Сессия')
+    basket = models.CharField(max_length=100, verbose_name='Корзина', null=True, blank=True)
